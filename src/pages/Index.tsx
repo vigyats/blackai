ce6787f913162f7d064c3b942b6181b62e256ea4
Vigyat Singh <vigyatsingh33@gmail.com>
2026-03-07 21:15:35 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -120,21 +120,6 @@ const Index = () => {
             </motion.div>
           </div>
         </div>
-
-        {/* Scroll indicator */}
-        <motion.div
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        >
-          <div className="w-6 h-10 rounded-full border-2 border-border/50 flex items-start justify-center p-2">
-            <motion.div
-              className="w-1 h-2 bg-muted-foreground rounded-full"
-              animate={{ y: [0, 12, 0] }}
-              transition={{ duration: 2, repeat: Infinity }}
-            />
-          </div>
-        </motion.div>
       </section>
 
       {/* Trusted By Section */}
