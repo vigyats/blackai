8eb86706a2c5d473ed7da79654a36ce2f20711fd
Vigyat Singh <vigyatsingh33@gmail.com>
2026-04-29 14:20:42 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -48,8 +48,8 @@ const team = [
   { name: 'Vigyat Singh', role: 'CEO & Co-founder', image: '/vigyat.png', linkedin: 'https://www.linkedin.com/in/vigyat-singh/' },
   { name: 'Vaishnavi Bais', role: 'CMO & Co-founder', image: '/vaishnavi.png', linkedin: 'https://www.linkedin.com/in/vaishnavibais20/' },
   { name: 'Shreyash Dhengekar', role: 'CFO, Advisor & Co-founder', image: '/shreyash.png', linkedin: 'https://www.linkedin.com/in/shreyash-dhengekar' },
-  { name: 'Tanaya Sur', role: 'CRO, BDM & Co-founder', image: '/tanaya.png', linkedin: 'https://www.linkedin.com/in/tanaya-pillewan-38370a256/' },
-  { name: 'Suraj', role: 'CTO & Co-founder', image: '/suraj.png', linkedin: 'https://www.linkedin.com/in/suraj-dudhe-70b16b209/' },
+  { name: 'Tanaya Pillewan', role: 'CRO, BDM & Co-founder', image: '/tanaya.png', linkedin: 'https://www.linkedin.com/in/tanaya-pillewan-38370a256/' },
+  { name: 'Suraj Dudhe', role: 'CTO & Co-founder', image: '/suraj.png', linkedin: 'https://www.linkedin.com/in/suraj-dudhe-70b16b209/' },
 ];
 
 const About = () => {
